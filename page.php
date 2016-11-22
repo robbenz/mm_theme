dab47c745184db7d552cdbc471ccfe5101214ce0
Rob Benz <benz_rob@yahoo.com>
2016-11-22 12:21:43 -0500

    setup home page to take includes/home-index.php

--- page.php
+++ page.php
@@ -1,19 +1,44 @@
 <?php get_header(); ?>
 
+<?php if ( is_home() || is_front_page() ): ?>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-sm-8">
+        <div id="content" role="main">
+          <?php get_template_part('includes/home-index', 'page'); ?>
+        </div><!-- /#content -->
+      </div>
+
+  <!--    <div class="col-sm-4" id="sidebar" role="navigation"> -->
+         <?php //   get_sidebar(); ?>
+  <!--     </div> -->
+
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+
+<?php else : ?>
+
+
 <div class="container">
   <div class="row">
-    
+
     <div class="col-sm-8">
       <div id="content" role="main">
         <?php get_template_part('loops/content', 'page'); ?>
       </div><!-- /#content -->
     </div>
-    
+
     <div class="col-sm-4" id="sidebar" role="navigation">
        <?php get_sidebar(); ?>
     </div>
-    
+
   </div><!-- /.row -->
 </div><!-- /.container -->
+
+
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
