6de0de6fb4383de9d78c90db991a24dda47d8eae
09jdphil <jphillips@diamedicalusa.com>
2016-12-02 15:29:50 -0500

    responsive footer

--- footer.php
+++ footer.php
@@ -1,12 +1,104 @@
-<footer class="container">
-  <hr/>
-	<div class="row">
-    <?php dynamic_sidebar('footer-widget-area'); ?>
-  </div>
-  <p class="text-center">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
-  </div>
-</footer>
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div id="footer2014">
+    <div id="footerwrap" class="container">
+      <div id="footerlogos" class="row">
+        <div id="diamedicallogo" class="col-xs-4">
+          <a href="<?php echo site_url(); ?>">
+            <img src="https://diamedicalusa.com/wp-content/imgs/footer-sample/diamedicallogosmall.png" />
+          </a>
+        </div>
+        <div id="medmatlogo" class="col-xs-4">
+          <a href="http://medmattress.com">
+            <img src="https://diamedicalusa.com/wp-content/imgs/footer-sample/medmattlogo.png" />
+          </a>
+        </div>
+        <div id="simlablogo" class="col-xs-4">
+          <a href="<?php echo site_url(); ?>/simlabsolutions/">
+            <img src="<?php echo site_url(); ?>/wp-content/imgs/footer-sample/simlablogo.png" />
+          </a>
+        </div>
+      </div>
+    </div> <!-- /container-->
+    <div id="logo-border" class="container">
+    </div>
+    <div id="footer-links" class="container">
+      <div class="row">
+      <div id="footerconnect" class="col-xs-3">
+        <p class="connecttop">p. (877) 593-6011<br />f.&nbsp;&nbsp;(248) 671-1550<br /></p>
+        <p class="connectbottom">Info@DiaMedicalUSA.com<br />5807 W. Maple, Suite #175, <br>West Bloomfield, MI 48322</p>
+     </div><!--footerconnect-->
+
+      <div id="footerlinks1" class="col-xs-3">
+        <a href="<?php echo site_url(); ?>">Home</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/">Mattresses</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/replacement-covers/">Replacement Covers</a><br />
+
+        <a href="#" class="eModal-2">Contact</a><br />
+        <a href="<?php echo site_url(); ?>/blog">Blog</a><br />
+        <a href="<?php echo site_url(); ?>/terms-conditions">Terms &amp; Conditions</a><br />
+        <a href="<?php echo site_url(); ?>/shipping-returns">Shipping &amp; Returns</a><br />
+      </div><!--footerlinks1-->
+      <div id="footerlinks2" class="col-xs-3">
+        <p class="popularproducts">Popular Products</p><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/stretcher-gurney/">Stretcher Pads</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/stretcher-gurney/">Stretcher Mattresses</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/med-surg-mattresses/">Med-Surg Mattresses</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/operating-table/">OR Table Pads</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/camp-dorm/">Camp Mattresses</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/camp-dorm/">Dormitory Mattresses</a><br />
+
+      </div><!--footerlinks2-->
+       <div id="footerlinks3" class="col-xs-3">
+        <p class="popularproducts">&nbsp;&nbsp;</p><br />
+        <a href="<?php echo site_url(); ?>/custom-sizing-form/">Custom Sizing</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/pressure-redistribution/">Wound Care Mattresses</a><br />
+        <a href="<?php echo site_url(); ?>/product-category/mattresses/labor-delivery/">Labor &amp; Delivery Mattresses</a><br />
+        <a href="http://www.diamedicalusa.com/hospital-beds-parts.php">Hospital Beds</a><br /><br />
+        <a href="http://www.diamedicalusa.com/home-services.php">Repairs</a><br />
+        <a href="http://www.diamedicalusa.com/parts.php">Replacement Parts</a><br />
+
+      </div><!--footerlinks3-->
+    </div> <!--footer-links-->
+    <div class="socialicons2014">
+      <a href="http://www.facebook.com/pages/DiaMedical-USA/231317596883279" target="_blank">
+        <img class="social-left" src="https://diamedicalusa.com/wp-content/imgs/social/facebook-hover.png" onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/facebook.png';" onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/facebook-hover.png';"/>
+      </a>
+      <a href="http://twitter.com/DiaMedicalUSA" target="_blank">
+        <img class="social-mid" src="https://diamedicalusa.com/wp-content/imgs/social/twitter-hover.png" onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/twitter.png';" onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/twitter-hover.png';"/>
+      </a>
+      <a href="http://www.linkedin.com/company/2439975?goback=.fcs_GLHD_diamedical+usa_false_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2&trk=ncsrch_hits" target="_blank">
+        <img class="social-mid" src="https://diamedicalusa.com/wp-content/imgs/social/linkedin-hover.png" onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/linkedin.png';" onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/linkedin-hover.png';"/>
+      </a>
+      <a href="http://www.pinterest.com/medmattress/" target="_blank">
+        <img class="social-mid" src="https://diamedicalusa.com/wp-content/imgs/social/pinterest-hover.png" onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/pinterest.png';" onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/pinterest-hover.png';"/>
+      </a>
+      <a href="http://diamedicalusa.com/blog/" target="_blank">
+        <img class="social-right" src="https://diamedicalusa.com/wp-content/imgs/social/wordpress-hover.png" onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/wordpress.png';" onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/wordpress-hover.png';"/>
+      </a>
+    </div><!--socialicons2014-->
+  </div><!--footer2014-->
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
+<!-- begin olark code -->
+<script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){
+f[z]=function(){
+(a.s=a.s||[]).push(arguments)};var a=f[z]._={
+},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){
+f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={
+0:+new Date};a.P=function(u){
+a.p[u]=new Date-a.p[0]};function s(){
+a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){
+hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){
+return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){
+b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{
+b.contentWindow[g].open()}catch(w){
+c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{
+var t=b.contentWindow[g];t.write(p());t.close()}catch(x){
+b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({
+loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+/* custom configuration goes here (www.olark.com/documentation) */
+olark.identify('7424-266-10-3934');/*]]>*/</script><noscript><a href="https://www.olark.com/site/7424-266-10-3934/contact" title="Contact us" target="_blank">Questions? Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat software</a></noscript>
+<!-- end olark code -->
 </body>
 </html>
