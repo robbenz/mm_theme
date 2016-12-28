a4c4865f678b9242d1f1c5f6133782c26b9da4a4
09jdphil <jphillips@DiaMedicalUSA.com>
2016-12-28 16:17:54 -0500

    header fix fix

--- theme/js/b4st.js
+++ theme/js/b4st.js
@@ -34,17 +34,6 @@
         /*** END ***/
 
         /*** Header classes for mobile Nav ***/
-        function viewport() {
-            var e = window, a = 'inner';
-            
-            if (!('innerWidth' in window )) {
-                a = 'client';
-                e = document.documentElement || document.body;
-            }
-            return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-        }
-
-        var vpWidth = viewport().width; // This should match your media query
 
         function addClassesMobile($when) {
             $(window).on($when, function() {
