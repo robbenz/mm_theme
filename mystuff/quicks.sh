4720d872765497ef4c01ff9ed41798eb364e17af
Rob Benz <benz_rob@yahoo.com>
2017-01-03 12:50:48 -0500

    todo flow

--- mystuff/quicks.sh
+++
@@ -1,4 +0,0 @@
-wp menu location assign home home 
-wp search-replace "background-image: url('../../wp-content/imgs/Category-Mattress-header.png');" ""
-wp search-replace "background-image: url('../../../wp-content/imgs/Category-Mattress-header.png');" ""
-wp search-replace "background-image: url('../../../../wp-content/imgs/Category-Mattress-header.png');" ""
