45c12eb872abff62a2f04c4e39f323fce1875ad7
09jdphil <jphillips@DiaMedicalUSA.com>
2017-01-04 16:18:06 -0500

    custom sizing form changes

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
 			<link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet">
 			<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 			<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-			<script type="text/javascript" src="<?php echo site_url(); ?>/wp-content/themes/medmattress/theme/js/b4st.js"></script>
 	<?php wp_head(); ?>
 	<script>
  jQuery( ".menu-item-1917" ).addClass( "menu-white-link" );
