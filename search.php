ba87a74df4d56005d13b80f784cd2491298634fd
Rob Benz <benz_rob@yahoo.com>
2017-01-16 08:29:44 -0500

    cleaned up search results -- for when you search something we dont have

--- search.php
+++ search.php
@@ -18,35 +18,32 @@
         echo '</ul>';
         mm_page_stuff();
       elseif (! have_posts() ):
-      //  echo 'No Results Found';
 
-        $params = array(
-        'posts_per_page' => 12,
+        $args = array(
+        'posts_per_page' => 20,
         'post_type' => 'product',
-        'product_cat' => 'mental-health-mattresses'
-
+        'product_cat' => 'mental-health-mattresses,labor-delivery,pressure-redistribution',
+        'orderby' => 'rand'
       );
-      $wc_query = new WP_Query($params); // (2)
+      $wc_query = new WP_Query($args);
         ?>
-        <?php if ($wc_query->have_posts()) : // (3) ?>
-          <div class="header-wrap-text-mattress">
-            <h2 class="header-wrap-text-mattress-header">
-              <span class="header4-spread_copy">No Results Found<br></span>
-              <p>Browse<br>Mental Healh Mattresses<br>Below</p>
-            </h2>
+        <?php if ($wc_query->have_posts()) :  ?>
+          <div style="height:13vw;" class="header-wrap-text-mattress">
+            <h2 style="color: #004ea8; text-transform: uppercase;font-size:3.2rem;">No Results Found</h2>
           </div>
+          <h3 style="width:100%;border-bottom:4px dotted #78be20;">Browse Our Most Popular Mattresses</h3>
         <?php echo '<ul class="products">';
-        while ($wc_query->have_posts()) : // (4)
-                        $wc_query->the_post(); // (4.1)
+        while ($wc_query->have_posts()) :
+                        $wc_query->the_post();
                             ?>
-        <?php wc_get_template_part( 'content', 'product' ); // (4.2) ?>
+        <?php wc_get_template_part( 'content', 'product' );  ?>
 
         <?php endwhile; ?>
           <?php echo '</ul>'; ?>
-        <?php wp_reset_postdata(); // (5) ?>
+        <?php wp_reset_postdata(); ?>
         <?php else:  ?>
         <p>
-             <?php _e( 'No Products' ); // (6) ?>
+             <?php _e( 'No Products' );  ?>
         </p>
         <?php endif;
 
