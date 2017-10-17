7b60eafa55385b8cd7f6d2bc72516ce8c5960363
DiaMedical USA User <dmuser@Michaels-iMac.local>
2017-10-17 11:37:18 -0400

    before merge

--- theme/js/b4st.js
+++ theme/js/b4st.js
@@ -173,6 +173,18 @@
           });
         /*** END ***/
 
+        $('#vfbField126').click(function() {
+           if($('#vfb-field-126-1').is(':checked')) {
+             $('#vfbField136 .vfb-col-10 .vfb-radio:nth-child(1)').show();
+             $('#vfbField136 .vfb-col-10 .vfb-radio:nth-child(2)').hide();
+           } else if($('#vfb-field-126-0').is(':checked')) {
+             $('#vfbField136 .vfb-col-10 .vfb-radio:nth-child(1)').show();
+             $('#vfbField136 .vfb-col-10 .vfb-radio:nth-child(2)').show();
+           }
+        });
+
+
+
         //  $(".custom-mat-wrapped-input").wrapAll("<div class='custom-mat-wrapped-input-wrapper'></div>");
 
 
